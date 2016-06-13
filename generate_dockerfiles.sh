b68e48149e49e2104ef4f1725c5f8d16b7c5e265
Anastas Dancha <anapsix@random.io>
2016-06-13 10:52:21 -0400

    GLIBC: upgrade to 2.23-r3, make it easier

--- generate_dockerfiles.sh
+++ generate_dockerfiles.sh
@@ -8,16 +8,18 @@ JDK_DCEVM_TEMPLATE="Dockerfile.jdk-dcevm.tpl"
 
 JAVA_VERSIONS=( 7-80-15 8-92-14 )
 
+GLIBC_VERSION="2.23-r3"
+
 for version in ${JAVA_VERSIONS[@]}; do
   JVM_MAJOR=$(echo $version | cut -d- -f1)
   JVM_MINOR=$(echo $version | cut -d- -f2)
   JVM_BUILD=$(echo $version | cut -d- -f3)
   echo -en "Generating Dockerfile for ${JVM_MAJOR}u${JVM_MINOR}b${JVM_BUILD} JRE.. "
-  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/server-jre/g" $JRE_TEMPLATE > $JVM_MAJOR/jre/Dockerfile && \
+  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/server-jre/g;s/%GLIBC_VERSION%/$GLIBC_VERSION/g" $JRE_TEMPLATE > $JVM_MAJOR/jre/Dockerfile && \
     echo "done" || \
     echo "failed"
   echo -en "Generating Dockerfile for ${JVM_MAJOR}u${JVM_MINOR}b${JVM_BUILD} JDK.. "
-  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/jdk/g" $JDK_TEMPLATE > $JVM_MAJOR/jdk/Dockerfile && \
+  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/jdk/g;s/%GLIBC_VERSION%/$GLIBC_VERSION/g" $JDK_TEMPLATE > $JVM_MAJOR/jdk/Dockerfile && \
     echo "done" || \
     echo "failed"
 
@@ -30,7 +32,7 @@ for version in ${JAVA_VERSIONS[@]}; do
 	  DCEVM_INSTALLER_NAME="DCEVM-light-8u74-installer.jar"
   fi
   
-  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/jdk/g;s/%DCEVM_INSTALLER_URL%/$DCEVM_INSTALLER_URL/g;s/%DCEVM_INSTALLER_NAME%/$DCEVM_INSTALLER_NAME/g" $JDK_DCEVM_TEMPLATE > $JVM_MAJOR/jdk-dcevm/Dockerfile && \
+  sed "s/%JVM_MAJOR%/$JVM_MAJOR/g;s/%JVM_MINOR%/$JVM_MINOR/g;s/%JVM_BUILD%/$JVM_BUILD/g;s/%JVM_PACKAGE%/jdk/g;s/%DCEVM_INSTALLER_URL%/$DCEVM_INSTALLER_URL/g;s/%DCEVM_INSTALLER_NAME%/$DCEVM_INSTALLER_NAME/g;s/%GLIBC_VERSION%/$GLIBC_VERSION/g" $JDK_DCEVM_TEMPLATE > $JVM_MAJOR/jdk-dcevm/Dockerfile && \
     echo "done" || \
     echo "failed"
 
